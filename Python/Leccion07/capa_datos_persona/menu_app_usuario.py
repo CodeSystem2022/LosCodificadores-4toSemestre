3f1ce75568656f7d6b8da8c48d3885d258d5b197
Cecilia Iribarren <cecirib@gmail.com>
2023-10-06 19:45:33 -0300

    Add: comenzamos las pruebas de la clase UsuarioDao y menu_app_usuario

--- Python/Leccion07/capa_datos_persona/menu_app_usuario.py
+++ Python/Leccion07/capa_datos_persona/menu_app_usuario.py
@@ -1,6 +1,15 @@
 
 from logger_base import log
 
+opcion = None
+while opcion != 5:
+    print('Opciones: ')
+    print('1. Listar Usuarios')
+    print('2. Agregar Usuario')
+    print('3. Modificar Usuario')
+    print('4. Eliminar Usuario')
+    print('5. salir')
+    opcion = int(input('Digite la opcion (1-5): '))
 
 
 
