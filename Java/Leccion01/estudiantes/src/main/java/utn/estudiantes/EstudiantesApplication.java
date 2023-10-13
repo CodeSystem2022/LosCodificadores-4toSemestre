357af0da0da1111ce9dc2e8bf9d0236bc7a4b51d
Cecilia Iribarren <cecirib@gmail.com>
2023-10-13 19:15:41 -0300

    Add: Abrimos el archivo en Intellij y sincronizamos el pomp con Maven

--- Java/Leccion01/estudiantes/src/main/java/utn/estudiantes/EstudiantesApplication.java
+++ Java/Leccion01/estudiantes/src/main/java/utn/estudiantes/EstudiantesApplication.java
@@ -1,13 +1,13 @@
-package utn.estudiantes;
+package utn.estudiantes;// paquete utn.estudiante
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class EstudiantesApplication {
+@SpringBootApplication // notacion para ejecutar
+public class EstudiantesApplication { //clase
 
 	public static void main(String[] args) {
 		SpringApplication.run(EstudiantesApplication.class, args);
-	}
+	}//todo esto es para poder ejecutar la aplicacion para poder ejecutar la aplicacion
 
 }
