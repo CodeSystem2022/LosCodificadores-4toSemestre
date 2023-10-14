5927fe535d2789ec850e3fa5bdbf136df858be0c
GuillermoEQ <qguillermoe@gmail.com>
2023-10-14 13:01:03 -0300

    Add: Se agregó el código al styles.css, product.js, y a index.js.

--- JavaScript/e-commerce2022/client/js/index.js
+++ JavaScript/e-commerce2022/client/js/index.js
@@ -1,27 +1,28 @@
-
+const shopContent = document.getElementById("shopContent");
 const cart = []; // El array vacío es el carrito.
 
+productos.forEach((product) =>{
+    const content = document.createElement("div");
+    content.innerHTML = `
+    <img src="${product.img}">
+    <h3>${product.productName}</h3>
+    <p>${product.price} $</p>
+    `;
+    shopContent.append(content);
+});
 
+const buyButton = document.createElement("button");
+buyButton.innerText = "Comprar";
 
+content.append(buyButton);
 
-
-
-
-
-
-
-    const buyButton = document.createElement("button");
-    buyButton.innerText = "Comprar";
-
-    content.append(buyButton);
-
-    buyButton.addEventListener("click", ()=>{
-        cart.push({
-            id: product.id,
-            productName: product.productName,
-            price: product.price,
-            quanty: prompt.quanty,
-            img: product.img,
-        })
-        console.log(cart)
+buyButton.addEventListener("click", ()=>{
+    cart.push({
+        id: product.id,
+        productName: product.productName,
+        price: product.price,
+        quanty: prompt.quanty,
+        img: product.img,
     })
+    console.log(cart)
+})    
