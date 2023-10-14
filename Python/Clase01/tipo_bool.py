09f06134f16895781589d8f21a620f15e2e8d4b1
Agustin Vera <ronedev25@gmail.com>
2023-10-14 19:56:44 -0300

    add: vemos las colecciones con bool

--- Python/Clase01/tipo_bool.py
+++ Python/Clase01/tipo_bool.py
@@ -17,3 +17,32 @@ print(f' valor: {valor}, Resultado: {resultado}')
 valor = 'Hola'
 resultado = bool(valor)
 print(f' valor: {valor}, Resultado: {resultado}')
+
+#Tipo colecciones - false para colecciones vacias
+#Tipo colecciones -  true para todas la demas
+#Lista
+valor = []
+resultado = bool(valor)
+print(f' valor de una lista vacia: {valor}, Resultado: {resultado}')
+
+valor = [2, 3, 5]
+resultado = bool(valor)
+print(f' valor de una lista con elementos: {valor}, Resultado: {resultado}')
+
+#Tupla
+valor = ()
+resultado = bool(valor)
+print(f' valor de una tupla vacia: {valor}, Resultado: {resultado}')
+
+valor = (5,)
+resultado = bool(valor)
+print(f' valor de una tupla con elementos: {valor}, Resultado: {resultado}')
+
+#Diccionario
+valor = {}
+resultado = bool(valor)
+print(f' valor de un diccionario vacio: {valor}, Resultado: {resultado}')
+
+valor = {"nombre": "Juan", "apellido": "Perez"}
+resultado = bool(valor)
+print(f' valor de un diccionario con elementos: {valor}, Resultado: {resultado}')
