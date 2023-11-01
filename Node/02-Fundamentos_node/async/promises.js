3a5caf6087061765080851c8d84d676ed35cd51e
Giuliana <giuliana9566@gmail.com>
2023-11-01 00:45:34 -0300

    Aplicando Promise en la funcion hola

--- Node/02-Fundamentos_node/async/promises.js
+++ Node/02-Fundamentos_node/async/promises.js
@@ -1,20 +1,30 @@
-function hola(nombre, miCallback){
-    setTimeout(function (){
+
+function hola(nombre){
+    return new Promise(function (resolve, reject){
+      setTimeout(function (){
         console.log('Hola '+nombre);
-        miCallback(nombre);
-    }, 1000)    
+        resolve(nombre);
+    }, 1000)      
+    });
+    
 }
 
-function hablar(callbackHablar){
-    setTimeout(function() {
+function hablar(nombre){
+    
+        setTimeout(function() {
         console.log('bla bla bla bla');
-        callbackHablar();
+        resolve(nombre);
     }, 1000);
+    });    
 }
 
-function adios(nombre, otroCallback){
-    setTimeout( function(){
+function adios(nombre){
+    
+       setTimeout( function(){
         console.log('Adios '+nombre);
-        otroCallback();
-    }, 1500);
+       // resolve();
+       reject('Hay un error');
+    }, 1000); 
+    });
+    
 }
