b8c2f39d1043a501671d574e70db7fd49ebfedd6
Nicolas Aranibar <aranibarnicolas596@gmail.com>
2023-11-02 10:54:26 -0300

    Metodo join parte 2

--- Python/Clase01/metodo_join.py
+++ Python/Clase01/metodo_join.py
@@ -5,3 +5,17 @@
 tupla_str = ('Hola', 'alumnos', 'Tecnicatura', 'Universitaria')
 mensaje = ' '.join(tupla_str)
 print(f'Mensaje: {mensaje}')
+
+lista_cursos = ['java', 'python', 'angular', 'spring']
+mensaje = ', '.join(lista_cursos)
+print(f'Mensaje: {mensaje}')
+
+cadena = 'HolaMundo'
+mensaje = '.'.join(cadena)
+print(f'Mensaje: {mensaje}')
+
+diccionario = {'nombre': 'juan', 'apellido'; 'Perez', 'edad': '18'}
+llaves = '-'.join(diccionario.keys())
+valores = '-'.join(diccionario.values())
+print(f'Llaves: {llaves}, Tyoe: {type(llaves)}')
+print(f'Valores: {Valores}, Type: {type(valores)}')
