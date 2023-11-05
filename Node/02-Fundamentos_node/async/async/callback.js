1d2571d176705ccda182fd7cd15811d2ad023b22
GuillermoEQ <qguillermoe@gmail.com>
2023-11-05 03:41:52 -0300

    Add: Nuevas carpetas y archivo callback. Generando asincronía con el setTimeout.

---
+++ Node/02-Fundamentos_node/async/async/callback.js
@@ -0,0 +1,10 @@
+function soyAsincrona(){
+    setTimeout(function(miCallback) {
+        console.log('Hola, soy una función asíncrona');
+    }, 1000);  
+}
+
+console.log('Iniciando el proceso...');
+soyAsincrona(function() {
+    console.log('Terminado el proceso...');
+});
