a98eb81df42bc5939463c6a8ada47de36da34b3f
GuillermoEQ <qguillermoe@gmail.com>
2023-11-05 04:13:23 -0300

    Add: Se agrega código más complejo. Uso de variables compartidas.

--- Node/02-Fundamentos_node/async/async/callback.js
+++ Node/02-Fundamentos_node/async/async/callback.js
@@ -1,11 +1,23 @@
-function soyAsincrona(niCallback){
+function hola(nombre, miCallback){
     setTimeout(function() {
-        console.log('Hola, soy una función asíncrona');
-        miCallback();
+        console.log('Hola '+nombre);
+        miCallback(nombre);
     }, 1000);  
 }
 
+function adios(nombre, otroCallback) {
+    setTimeout(function() {
+        console.log('Adios '+nombre)
+        otroCallback();
+    }, 1500);
+}
+
 console.log('Iniciando el proceso...');
-soyAsincrona(function() {
-    console.log('Terminado el proceso...');
+hola('Carlos', function(nombre) {
+    adios(nombre, function() {
+        console.log('Terminado el proceso...');
+    });  
 });
+
+//hola('Carlos', function(){});
+//adios('Carlos', function(){});
